5ef4ff7c7aa455b447c24f7c9014776e384d0452
Vivien Galuchot <vivien.galuchot@gmail.com>
2017-02-04 21:24:29 +0100

    Improvements

--- src/passgen/WordGenerator.java
+++ src/passgen/WordGenerator.java
@@ -9,7 +9,7 @@ public class WordGenerator extends Element<String>{
 		SingRandom.init();
 	}
 	
-	public String getPassword(int lenght){
+	public String getWord(int lenght){
 		String str = "";
 		Element<String> el = getNext();
 		while(str.length()<lenght && el != null){
@@ -21,10 +21,9 @@ public class WordGenerator extends Element<String>{
 
 	public void build(){
 		
-		String[] voyStr = {"a","e","i","o","u","y","ai","au","eau","é","è"};
+		String[] voyStr = {"a","e","i","o","u","y","ai","au","eau"};
 		String[] conStr = {"b","c","d","f","g","h","j","k","l","m","n","p","r","s","t","v","w","x","z"};
-		String[] conDouble = {"ss","tt","tr","mm","nn","ch","ck","sc","st","tr","pt","fr","cr","cl","br","bl"};
-		String[] syll = {"que","qua","qui","queau","queu","quo"};
+		String[] conDouble = {"qu","ss","tt","tr","mm","nn","ch","ck","sc","st","tr","pt","fr","cr","cl","br","bl"};
 
 		ArrayList<Element<String>> voyelles = new ArrayList<Element<String>>();
 		for(String str : voyStr){
@@ -44,17 +43,10 @@ public class WordGenerator extends Element<String>{
 			consonnesDoubles.add(el);
 		}
 		
-		ArrayList<Element<String>> syllabes = new ArrayList<Element<String>>();
-		for(String str : syll){
-			Element<String> el = new Element<String>(str);
-			syllabes.add(el);
-		}
-		
 		// set des nexts
 		for(Element<String> el : voyelles){
 			el.addAllToNext(consonnes);
 			el.addAllToNext(consonnesDoubles);
-			el.addAllToNext(syllabes);
 		}
 		
 		for(Element<String> el : consonnes){
@@ -65,21 +57,17 @@ public class WordGenerator extends Element<String>{
 			el.addAllToNext(voyelles);
 		}
 		
-		for(Element<String> el : syllabes){
-			el.addAllToNext(consonnes);
-		}
-		
 		// Ajout des lettres
 		next.clear();
 		addAllToNext(voyelles);
 		addAllToNext(consonnes);
-		addAllToNext(syllabes);
+		addAllToNext(consonnesDoubles);
 	}
 	
 	// Example d'utilisation
 	public static void main(String[] args) {
 		SingRandom.init();
 		WordGenerator gen = new WordGenerator();
-		System.out.println(gen.getPassword(8));
+		System.out.println(gen.getWord(8));
 	}
 }
